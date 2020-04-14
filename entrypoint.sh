8a3da950c2f12835217b689ef0dc4f65a7f368e2
Krzysztof Szyper <krzysztof.szyper@gmail.com>
2020-04-14 12:41:46 +0200

    Checkout branch before committing

--- entrypoint.sh
+++ entrypoint.sh
@@ -52,6 +52,7 @@ fi
 
 # Create an auto commit
 if [[ -z ${FILES_CHANGED} ]]; then
+  git checkout -b "${BRANCH}"
   git config --global user.name "${GITHUB_ACTOR}"
   git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
   git add -A
